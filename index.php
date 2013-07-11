12f7c7b1eba14ad448c15efc7449a6c7da8f0791
Zac Colley <zaccolley@gmail.com>
2013-07-12 00:18:09 +0100

    Google Analytics and meta shit

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
 	<title>Zac Colley</title>
+
+	<meta charset="UTF-8">
+	<meta name="description" content="Web developer and designer but also a pretty cool guy*. I am studying at the University of Portsmouth for Web Technologies, a Computer Science course for web fanatics. *Arguably...">
+	<meta name="keywords" content="zac colley, zac, colley, web development, web, development, dev">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
 	<link rel="shortcut icon" href="img/notepad.png" />
 	<link rel="stylesheet" href="css/style.css">
 
@@ -141,5 +145,15 @@
 <script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
 <script type="text/javascript" src="js/scripts.js"></script>
 <script type="text/javascript" src="js/prefixfree.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40429145-1', 'zaccolley.com');
+  ga('send', 'pageview');
+
+</script>
 </body>
 </html>
