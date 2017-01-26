449d011f5b4ad933bb1ad769515000e6be656eee
Zac Colley <hi@zaccolley.com>
2017-01-26 21:54:42 +0000

    Allow CORS

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ app.use(minifyHTML({
   }
 }))
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static(`${__dirname}/public`))
