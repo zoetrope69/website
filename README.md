5f43f1fa2e91d2c0fa9f2ae7b093740f15c7d9b7
Zac Colley <hi@zaccolley.com>
2019-02-28 22:16:30 +0000

    Replace website with Eleventy build

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# :tada: website!
+[![Netlify Status](https://api.netlify.com/api/v1/badges/cca05489-b5e5-453a-8c2d-01149b1d7f6f/deploy-status)](https://app.netlify.com/sites/zaccolley/deploys)
 
-my [current](http://www.zaccolley.com) website
+# zac.land
+
+my [current](https://zac.land) website
---
+++ _includes/layouts/base.11ty.js
@@ -0,0 +1,60 @@
+const base = data => {
+  const {
+    metadata,
+    page
+  } = data
+
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <title>${metadata.title} - ${metadata.description}</title>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="theme-color" content="#ddd">
+
+        <link rel="image_src" href="${metadata.author.image}">
+
+        <link rel="icon" sizes="192x192" href="/static/notepad.png">
+        <link rel="icon" href="/static/notepad.png" />
+
+        <link rel="stylesheet" href="/static/main.css">
+
+        <meta name="title" content="${metadata.title}">
+        <meta name="description" content="${metadata.description}">
+
+        <!-- Open Graph / Facebook -->
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="${metadata.url}${page.url}">
+        <meta property="og:title" content="${metadata.title}">
+        <meta property="og:description" content="${metadata.description}">
+        <meta property="og:image" content="${metadata.author.image}">
+
+        <!-- Twitter -->
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="twitter:url" content="${metadata.url}${page.url}">
+        <meta property="twitter:title" content="${metadata.title}">
+        <meta property="twitter:description" content="${metadata.description}">
+        <meta property="twitter:image" content="${metadata.author.image}">
+      </head>
+
+      <body>
+        <div class="wrapper">
+          <header>
+            <h1>
+              ${metadata.title}
+            </h1>
+            <p>${metadata.description}</p>
+          </header>
+
+          <main>
+            ${data.content}
+          </main>	
+        </div>
+      </body>
+    </html>
+  `;
+}
+
+module.exports = base
