3bdc299a381051843d5ef6881f5a26b8c68b8e26
Zac Colley <hi@zaccolley.com>
2019-07-28 18:25:22 +0100

    Add LastFM artists to website

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,23 @@
 const getStyles = require('./functions/styles');
+const getLastFMArtists = require('./functions/lastfm');
 
 module.exports = (eleventyConfig) => {
   // Copy different directories and files 
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("keybase.txt");
   eleventyConfig.addPassthroughCopy("manifest.json");
-  eleventyConfig.addPassthroughCopy("service-worker.js");
 
   eleventyConfig.addJavaScriptFunction("getStyles", getStyles);
+
+  eleventyConfig.addShortcode("latestLastFmArtists", async function() {
+    const artists = await getLastFMArtists();
+
+    if (!artists || artists.length === 0) {
+      return '';
+    }
+
+    return ` I've been listening to ${artists}.`;
+  });
 
   return {
     passthroughFileCopy: true
