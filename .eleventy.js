2282bdd67f1443358efb5671185998658c437d56
Zac Colley <hi@zaccolley.com>
2019-10-15 17:32:09 +0100

    Add letterboxd film to website
    Remove Goodreads

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const getStyles = require('./functions/styles');
 const getLastFMArtists = require('./functions/lastfm');
+const getLatestLetterboxDiaryEntry = require('./functions/letterboxd');
 
 module.exports = (eleventyConfig) => {
   // Copy different directories and files 
@@ -19,6 +20,20 @@ module.exports = (eleventyConfig) => {
     }
 
     return ` I've been listening to ${artists}.`;
+  });
+  eleventyConfig.addShortcode("latestLetterboxdFilm", async function() {
+    const letterboxdDiaryEntry = await getLatestLetterboxDiaryEntry();
+
+    if (!letterboxdDiaryEntry) {
+      return '';
+    }
+
+    const { film, rating, uri } = letterboxdDiaryEntry;
+
+    const lastFilmString = `The last film I watched was ${film.title}`;
+    const ratingString = `<a href="${uri}">I rated it <span aria-label="${rating.score}/5 stars">${rating.text}</span></a>`;
+
+    return ` ${lastFilmString}, ${ratingString}.`;
   });
 
   return {
